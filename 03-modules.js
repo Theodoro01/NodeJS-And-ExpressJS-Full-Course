7a8c85b244cf39d2f3ad65e4796bf1ca858f276c
Theodoro01 <theodorovieira01@gmail.com>
2021-10-08 14:10:25 -0300

    feat: NodeJs-Express-Course

--- 03-modules.js
+++ 03-modules.js
@@ -4,6 +4,7 @@ const names = require('./04-names')
 const sayHi = require('./05-utils')
 const data = require('./06-alternative-flavor')
 require('./07-mind-grenade')
+
 sayHi('susan')
 sayHi(names.john)
 sayHi(names.peter)
--- 07-mind-grenade.js
+++ 07-mind-grenade.js
@@ -1,8 +1,8 @@
-const num1 = 5
-const num2 = 10
+const num1 = 5;
+const num2 = 10;
 
-function addValues() {
-  console.log(`the sum is : ${num1 + num2}`)
+function addValues(){
+  console.log(`The sum is : ${num1 + num2}`);
 }
 
 addValues()
