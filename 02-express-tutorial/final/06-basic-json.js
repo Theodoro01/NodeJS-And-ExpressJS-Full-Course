80bf53eb8931f85ec08677f31ba5882bf68eda47
Theodoro01 <theodorovieira01@gmail.com>
2021-11-03 19:02:18 -0300

    feat: NodeJs-Express-Course

---
+++ 02-express-tutorial/final/06-basic-json.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const { products } = require('./data')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json(products)
+
+})
+
+app.listen(5000, () => {
+  console.log('APP RODANDO!')
+})
